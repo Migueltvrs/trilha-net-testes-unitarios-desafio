63907740eb52200d414cf4461f02d2765a130453
Miguel Tavares <migueltvrsg@gmail.com>
2023-12-20 11:29:59 -0300

    Desafio concluído

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -38,43 +38,55 @@ public class ValidacoesListaTests
     public void NaoDeveConterONumero10NaLista()
     {
         //TODO: Implementar método de teste
+        // Ok
 
         // Arrange
         var lista = new List<int> { 5, -1, -8, 9 };
         var numeroParaProcurar = 10;
 
         // Act
+        var resultado = _validacoes.ListaContemDeterminadoNumero(lista, numeroParaProcurar); //
 
         // Assert
+        Assert.False(resultado); // obs: não vai achar o número 10, logo, retornará verdadeiro.
     }
 
     //TODO: Corrigir a anotação [Fact]
+    // Ok
+    [Fact] //
     public void DeveMultiplicarOsElementosDaListaPor2()
     {
         //TODO: Implementar método de teste
+        // Ok
 
         // Arrange
         var lista = new List<int> { 5, 7, 8, 9 };
         var resultadoEsperado = new List<int> { 10, 14, 16, 18 };
         
         // Act
+        var listaMultiplicadaPor2 = _validacoes.MultiplicarNumerosLista(lista, 2); //
 
         // Assert
+        Assert.Equal(resultadoEsperado, listaMultiplicadaPor2); //
     }
 
     [Fact]
     public void DeveRetornar9ComoMaiorNumeroDaLista()
     {
         //TODO: Implementar método de teste
+        // Ok
 
         // Arrange
         var lista = new List<int> { 5, -1, -8, 9 };
+        var resultadoEsperado = 9; //
 
         // Act
+        var listaEsperadoRetornar9 = _validacoes.RetornarMaiorNumeroLista(lista); //
 
-        // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(9, 9);
+        // Ok
+        // Assert
+        Assert.Equal(resultadoEsperado, listaEsperadoRetornar9); //
     }
 
     [Fact]
@@ -84,12 +96,14 @@ public class ValidacoesListaTests
 
         // Arrange
         var lista = new List<int> { 5, -1, -8, 9 };
+        var resultadoEsperado = -8; //
 
         // Act
         var resultado = _validacoes.RetornarMenorNumeroLista(lista);
 
-        // Assert
+        
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(-8, -8);
+        // Assert
+        Assert.Equal(resultadoEsperado, resultado);
     }
 }
--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -10,10 +10,10 @@ public class ValidacoesStringTests
     public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
     {
         //TODO: Corrigir a variável "texto" e "resultadoEsperado" da seção Arrange
-
+        // Ok
         // Arrange
-        var texto = "a";
-        var resultadoEsperado = 0;
+        var texto = "Matrix"; //
+        var resultadoEsperado = 6; //
 
         // Act
         var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
@@ -30,12 +30,14 @@ public class ValidacoesStringTests
         var textoProcurado = "qualquer";
 
         //TODO: Corrigir a chamada do método "ContemCaractere" da seção Act
+        // Ok
         // Act
-         _validacoes.ContemCaractere(texto, textoProcurado);
+         var procurandoAPalavraQualquer = _validacoes.ContemCaractere(texto, textoProcurado); //
 
         // Assert
         //TODO: Corrigir o Assert.True com base no retorno da chamada ao método
-        Assert.True(true);
+        // Ok
+        Assert.True(procurandoAPalavraQualquer); //
     }
 
     [Fact]
@@ -50,17 +52,22 @@ public class ValidacoesStringTests
 
         // Assert
         //TODO: Corrigir o Assert.False com base no retorno da chamada ao método
-        Assert.False(true);
+        // Ok
+        Assert.False(resultado); // não vai achar a palavra "teste"(var resultado = false), 
+                                 // logo, a var resultado vai satistazer a condição do Assert.Faslse()
     }
 
     //TODO: Corrigir a anotação [Fact]
+    // Ok
+    [Fact] //
     public void TextoDeveTerminarComAPalavraProcurado()
     {
         //TODO: Corrigir a variável "textoProcurado" seção Arrange
+        // Ok
 
         // Arrange
         var texto = "Começo, meio e fim do texto procurado";
-        var textoProcurado = "teste";
+        var textoProcurado = "procurado"; // 
 
         // Act
         var resultado = _validacoes.TextoTerminaCom(texto, textoProcurado);
